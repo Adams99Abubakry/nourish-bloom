4f9cd01edfd9427c833114fa2fa2085556e9b15c
Adams99Abubakry <adamsabubakrsiddique@gmail.com>
2026-02-04 18:34:27 +0100

    iOS build prep

---
+++ android/app/src/main/java/com/siddiquedev/nurulislam/MainActivity.java
@@ -0,0 +1,5 @@
+package com.siddiquedev.nurulislam;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.siddiquedev.nurulislam',
+  appName: 'Nūr al-Islam',
+  webDir: 'dist'
+};
+
+export default config;
