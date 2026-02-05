9f7fe4a699b376d48e12d2e35e4ccb6582711e06
Vickygreene123 <okereaforfavour867@gmail.com>
2026-02-05 01:55:53 +0100

    match bundle Id on appstore with config file

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.siddiquedev.nurulislam',
+  appId: 'com.nural.islam',
   appName: 'Nūr al-Islam',
   webDir: 'dist'
 };
