fc6c166372d0af07156bda68630f7f2b43d3638b
Vickygreene123 <okereaforfavour867@gmail.com>
2026-02-06 02:16:22 +0100

    fix: add missing comma in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.nural.islam',
   appName: 'Nūr al-Islam',
-  webDir: 'dist'
+  webDir: 'dist',
+  
 };
 
 export default config;
